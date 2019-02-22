4b9296963f3ec2696bd82264c00fda2e2d1bc8cf
zhangzhian <zhangzhian_123@qq.com>
2019-02-22 19:38:11 +0800

    base

---
+++ Base21090222/base_.R
@@ -0,0 +1,311 @@
+# 单元素矢量
+# Atomic vector of type character.
+print("abc");
+
+# Atomic vector of type double.
+print(12.5)
+
+# Atomic vector of type integer.
+print(63L)
+
+# Atomic vector of type logical.
+print(TRUE)
+
+# Atomic vector of type complex.
+print(2+3i)
+
+# Atomic vector of type raw.
+print(charToRaw('hello'))
+
+
+#多元素向量
+# Creating a sequence from 5 to 13.
+v <- 5:13
+print(v)
+
+# Creating a sequence from 6.6 to 12.6.
+v <- 6.6:12.6
+print(v)
+
+# If the final element specified does not belong to the sequence then it is discarded.
+v <- 3.8:11.4
+print(v)
+# 序列(Seq.)运算符
+print(seq(5,9,by = 0.4))
+# The logical and numeric values are converted to characters.
+s <- c('apple','red',5,TRUE)
+print(s)
+
+
+# 访问向量元素
+# Accessing vector elements using position.
+t <- c("Sun","Mon","Tue","Wed","Thurs","Fri","Sat")
+u <- t[c(2,3,6)]
+print(u)
+
+# Accessing vector elements using logical indexing.
+v <- t[c(TRUE,FALSE,FALSE,FALSE,FALSE,TRUE,FALSE)]
+print(v)
+
+# Accessing vector elements using negative indexing.
+x <- t[c(-2,-5)]
+# print(c(-2,-5))
+print(x)
+
+# Accessing vector elements using 0/1 indexing.
+y <- t[c(0,0,0,0,0,0,1)]
+print(y)
+
+
+# Create two vectors.
+v1 <- c(3,8,4,5,0,11)
+v2 <- c(4,11,0,8,1,2)
+
+# Vector addition.
+add.result <- v1+v2
+print(add.result)
+
+# Vector substraction.
+sub.result <- v1-v2
+print(sub.result)
+
+# Vector multiplication.
+multi.result <- v1*v2
+print(multi.result)
+
+# Vector division.
+divi.result <- v1/v2
+print(divi.result)
+# 如果我们对两个不等长度的向量应用算术运算，则将循环使用较短向量的元素来完成操作。
+v1 <- c(3,8,4,5,0,11)
+v2 <- c(4,11)
+# V2 becomes c(4,11,4,11,4,11)
+
+add.result <- v1+v2
+print(add.result)
+
+sub.result <- v1-v2
+print(sub.result)
+# 向量元素排序
+# 可以使用sort()函数对向量中的元素进行排序。
+v <- c(3,8,4,5,0,11, -9, 304)
+
+# Sort the elements of the vector.
+sort.result <- sort(v)
+print(sort.result)
+
+# Sort the elements in the reverse order.
+revsort.result <- sort(v, decreasing = TRUE)
+print(revsort.result)
+
+# Sorting character vectors.
+v <- c("Red","Blue","yellow","violet")
+sort.result <- sort(v)
+print(sort.result)
+
+# Sorting character vectors in reverse order.
+revsort.result <- sort(v, decreasing = TRUE)
+print(revsort.result)
+
+
+# Create a list containing strings, numbers, vectors and a logical values.
+list_data <- list("Red", "Green", c(21,32,11), TRUE, 51.23, 119.1);
+print(list_data)
+# Create a list containing a vector, a matrix and a list.
+list_data <- list(c("Jan","Feb","Mar"), matrix(c(3,9,5,1,-2,8), nrow = 2),
+                  list("green",12.3));
+
+# Give names to the elements in the list.
+names(list_data) <- c("1st Quarter", "A_Matrix", "A Inner list");
+
+# Show the list.
+print(list_data);
+# Access the first element of the list.
+print(list_data[1])
+
+# Access the thrid element. As it is also a list, all its elements will be printed.
+print(list_data[3])
+
+# Access the list element using the name of the element.
+print(list_data$A_Matrix)
+
+# Add element at the end of the list.
+list_data[4] <- "New element"
+print(list_data[4])
+
+# Remove the last element.
+list_data[4] <- NULL
+
+# Print the 4th Element.
+print(list_data[4])
+
+# Update the 3rd Element.
+list_data[3] <- "updated element"
+print(list_data[3])
+
+# Create two lists.
+list1 <- list(1,2,3)
+list2 <- list("Sun","Mon","Tue")
+
+# Merge the two lists.
+merged.a <- c(list1,list2)
+
+# Print the merged list.
+print(merged.a)
+
+
+# Create lists.
+list1 <- list(1:5)
+print(list1)
+
+list2 <-list(10:14)
+print(list2)
+
+# Convert the lists to vectors.
+v1 <- unlist(list1)
+v2 <- unlist(list2)
+
+print(v1)
+print(v2)
+
+# Now add the vectors
+result <- v1+v2
+print(result)
+# R语言矩阵语法
+# -matrix(data, nrow, ncol, byrow, dimnames)
+# data - 是将要转为矩阵元素的输入向量。
+# nrow - 是要创建的行数。
+# ncol - 是要创建的列数。
+# byrow - 是一个逻辑线索。 如果为TRUE，则输入向量元素按行排列。
+# dimname - 是分配给行和列的名称。
+
+# Elements are arranged sequentially by row.
+M <- matrix(c(3:14), nrow = 4, byrow = TRUE)
+print(M)
+
+# Elements are arranged sequentially by column.
+N <- matrix(c(3:14), nrow = 4, byrow = FALSE)
+print(N)
+
+# Define the column and row names.
+rownames = c("row1", "row2", "row3", "row4")
+colnames = c("col1", "col2", "col3")
+
+P <- matrix(c(3:14), nrow = 4, byrow = TRUE, dimnames = list(rownames, colnames))
+print(P)
+
+# Access the element at 3rd column and 1st row.
+print(P[1,3])
+
+# Access the element at 2nd column and 4th row.
+print(P[4,2])
+
+# Access only the  2nd row.
+print(P[2,])
+
+# Access only the 3rd column.
+print(P[,3])
+
+# Create two 2x3 matrices.
+matrix1 <- matrix(c(3, 9, -1, 4, 2, 6), nrow = 2)
+print(matrix1)
+
+matrix2 <- matrix(c(5, 2, 0, 9, 3, 4), nrow = 2)
+print(matrix2)
+
+# Add the matrices.
+result <- matrix1 + matrix2
+cat("Result of addition","\n")
+print(result)
+
+# Subtract the matrices
+result <- matrix1 - matrix2
+cat("Result of subtraction","\n")
+print(result)
+
+# Multiply the matrices.
+result <- matrix1 * matrix2
+cat("Result of multiplication","\n")
+print(result)
+
+# Divide the matrices
+result <- matrix1 / matrix2
+cat("Result of division","\n")
+print(result)
+
+# 数组
+
+# Create two vectors of different lengths.
+vector1 <- c(5,9,3)
+vector2 <- c(10,11,12,13,14,15)
+
+# Take these vectors as input to the array.
+result <- array(c(vector1,vector2),dim = c(3,3,2))
+print(result)
+
+column.names <- c("COL1","COL2","COL3")
+row.names <- c("ROW1","ROW2","ROW3")
+matrix.names <- c("Matrix1","Matrix2")
+
+# Take these vectors as input to the array.
+result <- array(c(vector1,vector2),dim = c(3,3,2),dimnames = list(row.names,column.names,
+                                                                  matrix.names))
+print(result)
+
+# Print the third row of the second matrix of the array.
+print(result[3,,2])
+
+# Print the element in the 1st row and 3rd column of the 1st matrix.
+print(result[1,3,1])
+
+# Print the 2nd Matrix.
+print(result[,,2])
+
+# Take these vectors as input to the array.
+array1 <- array(c(vector1,vector2),dim = c(3,3,2))
+
+# Create two vectors of different lengths.
+vector3 <- c(9,1,0)
+vector4 <- c(6,0,11,3,14,1,2,6,9)
+array2 <- array(c(vector3,vector4),dim = c(3,3,2))
+
+# create matrices from these arrays.
+matrix1 <- array1[,,2]
+matrix2 <- array2[,,2]
+
+# Add the matrices.
+result <- matrix1+matrix2
+print(result)
+
+# 跨数组元素的计算
+# 我们可以使用apply()函数对数组中的元素进行计算
+# apply(x, margin, fun)
+# x - 是一个数组。
+# margin - 是使用的数据集的名称。
+# fun - 是应用于数组元素的函数。
+# Create two vectors of different lengths.
+
+# Take these vectors as input to the array.
+new.array <- array(c(vector1,vector2),dim = c(3,3,2))
+print(new.array)
+
+# Use apply to calculate the sum of the rows across all the matrices.
+result <- apply(new.array, c(1), sum)
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
